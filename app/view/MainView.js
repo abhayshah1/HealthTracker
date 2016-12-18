928e4bee446492c631234e2b504890646c41ab89
abhayshah1 <net_abhay@yahoo.com>
2016-12-17 23:06:16 -0500

    Dynamically draw series for all vitals

--- app/view/MainView.js
+++ app/view/MainView.js
@@ -97,6 +97,38 @@ Ext.define('HealthTracker.view.MainView', {
                     items: [
                         {
                             xtype: 'form',
+                            toggleChartSeries: function(checked, checkbox, chart) {
+                                var checkboxId = checkbox.id;
+                                var seriesName = checkboxId.concat('Series');
+
+                                if ( checked ) {
+                                    //create a series for the selected checkbox
+                                    var series = {
+                                        type: 'line',
+                                        itemId: seriesName,
+                                        id: seriesName,
+                                        seriesId: seriesName,
+                                        axis: 'left',
+                                        xField: 'visitDate',
+                                        yField: checkboxId,
+                                        smooth: 3
+                                    };
+
+                                    // add the series
+                                    chart.series.add( series );
+                                }
+                                else {
+                                    var series = chart.series.getByKey(seriesName);
+
+                                    // remove the series
+                                    chart.series.remove(series);
+                                }
+
+                                // redraw the chart
+                                chart.surface.removeAll();
+                                chart.redraw();
+                            },
+                            id: 'vitalsForm',
                             itemId: 'vitalsForm',
                             bodyPadding: 10,
                             layout: {
@@ -155,51 +187,56 @@ Ext.define('HealthTracker.view.MainView', {
                                         {
                                             xtype: 'checkboxfield',
                                             handler: function(checkbox, checked) {
+                                                // Get the chart to update
                                                 var vitalsLineChart = Ext.getCmp("vitalsLineChart");
 
-                                                if ( checked ) {
-                                                    //create a series for the selected checkbox
-                                                    var weightSeries = {
-                                                        type: 'line',
-                                                        itemId: 'weightSeries',
-                                                        id: 'weightSeries',
-                                                        seriesId: 'weightSeries',
-                                                        axis: 'left',
-                                                        xField: 'visitDate',
-                                                        yField: 'weight',
-                                                        smooth: 3
-                                                    };
+                                                // Assume one form by that name
+                                                var vitalsForm = Ext.ComponentQuery.query('#vitalsForm')[0];
 
-                                                    // add the series
-                                                    vitalsLineChart.series.add( weightSeries );
-                                                }
-                                                else {
-                                                    var weightSeries = vitalsLineChart.series.getByKey('weightSeries');
-                                                    vitalsLineChart.series.remove(weightSeries);
-
-                                                    // remove the series
-                                                    vitalsLineChart.series.remove(weightSeries);
-                                                }
-
-                                                // redraw the chart
-                                                vitalsLineChart.surface.removeAll();
-                                                vitalsLineChart.redraw();
+                                                vitalsForm.toggleChartSeries( checked, checkbox, vitalsLineChart );
                                             },
                                             id: 'weight',
                                             boxLabel: 'Weight'
                                         },
                                         {
                                             xtype: 'checkboxfield',
+                                            handler: function(checkbox, checked) {
+                                                // Get the chart to update
+                                                var vitalsLineChart = Ext.getCmp("vitalsLineChart");
+
+                                                // Assume one form by that name
+                                                var vitalsForm = Ext.ComponentQuery.query('#vitalsForm')[0];
+
+                                                vitalsForm.toggleChartSeries( checked, checkbox, vitalsLineChart );
+                                            },
                                             id: 'vitaminB12',
                                             boxLabel: 'Vitamin B12'
                                         },
                                         {
                                             xtype: 'checkboxfield',
+                                            handler: function(checkbox, checked) {
+                                                // Get the chart to update
+                                                var vitalsLineChart = Ext.getCmp("vitalsLineChart");
+
+                                                // Assume one form by that name
+                                                var vitalsForm = Ext.ComponentQuery.query('#vitalsForm')[0];
+
+                                                vitalsForm.toggleChartSeries( checked, checkbox, vitalsLineChart );
+                                            },
                                             id: 'Triglycerides',
                                             boxLabel: 'Triglycerides'
                                         },
                                         {
                                             xtype: 'checkboxfield',
+                                            handler: function(checkbox, checked) {
+                                                // Get the chart to update
+                                                var vitalsLineChart = Ext.getCmp("vitalsLineChart");
+
+                                                // Assume one form by that name
+                                                var vitalsForm = Ext.ComponentQuery.query('#vitalsForm')[0];
+
+                                                vitalsForm.toggleChartSeries( checked, checkbox, vitalsLineChart );
+                                            },
                                             id: 'vitaminD',
                                             boxLabel: 'VitaminD'
                                         }
@@ -213,36 +250,6 @@ Ext.define('HealthTracker.view.MainView', {
         });
 
         me.callParent(arguments);
-    },
-
-    toggleChartSeries: function(checked, seriesId, chart) {
-        if ( checked ) {
-            //create a series for the selected checkbox
-            var weightSeries = {
-                type: 'line',
-                itemId: 'weightSeries',
-                id: 'weightSeries',
-                seriesId: 'weightSeries',
-                axis: 'left',
-                xField: 'visitDate',
-                yField: 'weight',
-                smooth: 3
-            };
-
-            // add the series
-            vitalsLineChart.series.add( weightSeries );
-        }
-        else {
-            var weightSeries = vitalsLineChart.series.getByKey('weightSeries');
-            vitalsLineChart.series.remove(weightSeries);
-
-            // remove the series
-            vitalsLineChart.series.remove(weightSeries);
-        }
-
-        // redraw the chart
-        vitalsLineChart.surface.removeAll();
-        vitalsLineChart.redraw();
     }
 
 });
